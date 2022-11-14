e2916b9ac4a33aecd3171db63fe945998358ffdc
Efrain Castro Jr <efraincastrojrGmail.com>
2022-11-13 21:12:46 -0600

    added more test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,6 +1,30 @@
-const employee = requirer("../Employee");
+const Employee = require('../lib/Employee');
 
-test("can create a new employee object", () => {
+test('can create a new employee object', () => {
   const employee = new Employee();
-  expect(typeof employee.toBe("object"));
+  expect(typeof(employee)).toBe('object');
 });
+
+test('Can set name in epmloyee object', () => {
+  const employee = new Employee("Efrain");
+  expect(employee.name).toBe("Efrain");
+
+});
+
+test('Can set emplyee_id in epmloyee object', () => {
+  const employee = new Employee("Efrain", 13);
+  expect(employee.employee_id).toBe(13);
+
+});
+
+test('Can set email in epmloyee object', () => {
+  const employee = new Employee("Efrain", 13, "efraincastrojr@gmail.com");
+  expect(employee.email).toBe("efraincastrojr@gmail.com");
+
+});
+
+test('Can get name from getName() method', () => {
+  const employee = new Employee("Efrain", 13, "efraincastrojr@gmail.com");
+  expect(employee.getName()).toBe("Efrain");
+
+}); 
