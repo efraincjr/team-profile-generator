280ed1ab956e18efc7bd36424ca586d55ca314ef
Efrain Castro Jr <efraincastrojrGmail.com>
2022-12-21 11:31:33 -0600

    added more prompts

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
 
 const templates = require("./src/templates");
 
@@ -26,6 +27,7 @@ function main() {
       managerOfficeNumber
     );
     team.push(manager);
+    createTeam()
   });
 }
 
@@ -55,6 +57,31 @@ function createManager() {
   ]);
 }
 
+
+function createEngineer() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "engineerName",
+      message: "What engineer's name?",
+    },
+    {
+      type: "input",
+      name: "engineerId",
+      message: "what is engineers id?",
+    },
+    {
+      type: "input",
+      name: "engineerEmail",
+      message: "what is engineers email?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "what is the engineers github username?",
+    },
+  ]);
+}
 function createTeam() {
   inquirer
     .prompt([
@@ -67,7 +94,24 @@ function createTeam() {
     ])
     .then((answer) => {
       switch (answer.teamMemberChoice) {
-        case "Engineer":
+        case "Engineer": 
+        createEngineer().then(answers => {
+          const {
+            engineerName,
+            engineerId,
+            engineerEmail,
+            github,
+          } = answers;
+          const engineer = new Engineer(
+            engineerName,
+            engineerId,
+            engineerEmail,
+            github,
+          );
+          team.push(engineer);
+          createTeam();
+          
+        })
           break;
         case "Intern":
           break;
@@ -77,13 +121,17 @@ function createTeam() {
     });
 }
 function createHTML(teamArr) {
-  const htmlMain = ``;
+  let htmlMain = ``;
 
   teamArr.forEach((teamMember) => {
     if (teamMember.getRole() === "Manager") {
       htmlMain = htmlMain + templates.managerTemplate(teamMember);
     }
+    if (teamMember.getRole() === "Engineer") {
+      htmlMain = htmlMain + templates.engineerTemplate(teamMember);
+    }
   });
+ return htmlMain
 }
 function buildTeam() {
   fs.writeFileSync(PATH, createHTML(team), 'uta-8');
--- src/templates.js
+++ src/templates.js
@@ -1,16 +1,49 @@
 const engineerTemplate = engineer => {
-    ``
-}
+  const name = engineer.name;
+  const id = engineer.employee_id;
+  const email = engineer.email;
+  const role = engineer.getRole();
+  const github = engineer.getGitHub();
+
+  return `
+    <h2> ${role.toUpperCase()} </h2>
+    <div>Name: ${name}</div>
+    <div>Email: ${email}</div>
+    <div>Employee Id:${id}</div>
+    <div>Github: ${github} </div>
+    `
+};
 
 const managerTemplate = manager => {
-const name = manager.name;
-const id = manager.employee_id;
+  const name = manager.name;
+  const id = manager.employee_id;
+  const email = manager.email;
+  const officeNumber = manager.getOfficeNumber();
+  const role = manager.getRole();
 
-    return `
-    <div>Manager:${name}</div>
-    <div>Manager Id;${id}</div>
+  return `
+    <h2> ${role.toUpperCase()} </h2>
+    <div>Name: ${name}</div>
+    <div>Email: ${email}</div>
+    <div>Employee Id:${email}</div>
+    <div>Office Number: ${officeNumber}</div>
 
     `
-}
+};
 
-module.exports = {engineerTemplate, managerTemplate}
+const internTemplate = intern => {
+    const name = intern.name;
+    const id = intern.employee_id;
+    const email = intern.email;
+    const role = intern.getRole();
+    const school = intern.getSchool();
+  
+    return `
+      <h2> ${role.toUpperCase()} </h2>
+      <div>Name: ${name}</div>
+      <div>Email: ${email}</div>
+      <div>Employee Id:${id}</div>
+      <div>School: ${school} </div>
+      `
+}
+module.exports = { engineerTemplate, managerTemplate };
